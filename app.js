adc1178552173870cf9f76f721929d63fe4ea8a3
AngelDarling <nguyenhuadainam@gmail.com>
2025-02-25 01:51:38 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 
 const app = express();
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 app.use(cors());
 app.use(express.json());
@@ -13,5 +14,17 @@ app.get("/", (req, res) => {
 
 
 app.use("/api/contacts", contactsRouter);
+
+// Middleware xử lý lỗi 404
+app.use((req, res, next) => {
+    next(new ApiError(404, "Resource not found"));
+});
+
+// Middleware xử lý lỗi tập trung
+app.use((err, req, res, next) => {
+    res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
